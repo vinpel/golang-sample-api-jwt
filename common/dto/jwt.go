44d8df766a89989143e1206ba7a9773f4eca5133
Vincent Pélisset <vinpel@hotmail.com>
2022-10-29 00:10:04 +0200

    :bug: now usethe same demo account for : * User creation * the jwt from User

--- common/dto/jwt.go
+++ common/dto/jwt.go
@@ -1,5 +1,6 @@
 package dto
+
 type TokenRequest struct {
-	Email    string `json:"email"  example:"demo@exemple.net"`
+	Email    string `json:"email"  example:"demo@example.net"`
 	Password string `json:"password"  example:"demo"`
 }
--- common/dto/user.go
+++ common/dto/user.go
@@ -3,6 +3,6 @@ package dto
 type UserRequest struct {
 	Name     string `json:"name" example:"John Doe"`
 	Username string `json:"username" example:"john"`
-	Email    string `json:"email" example:"demo@http://example.com/"`
+	Email    string `json:"email" example:"demo@example.com"`
 	Password string `json:"password" example:"demo"`
 }
